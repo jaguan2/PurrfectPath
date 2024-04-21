170642360df2c45b8db2bb510880dc9a268d1bc5
Kelly Ton <kthton@gmail.com>
2024-04-21 15:14:58 -0400

    updated models and views

--- website/models.py
+++ website/models.py
@@ -8,3 +8,36 @@ class Student(db.Model, UserMixin):
     password = db.Column(db.String())
     username = db.Column(db.String())
     major = db.Column(db.String()) 
+
+# class Student(db.Model, UserMixin):
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(100))
+#     password = db.Column(db.String(100))
+#     username = db.Column(db.String(100))
+#     major = db.Column(db.String(100))
+#     advisor = db.Column(db.Integer, db.ForeignKey(Faculty.id))
+
+class Faculty(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(100))
+    lname = db.Column(db.String(100))
+    department = db.Column(db.String(100))
+
+class Courses(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(3))
+    courseno = db.Column(db.Integer)
+    title = db.Column(db.String(100))
+    credits = db.Column(db.Integer) 
+    instrumeth = db.Column(db.String(100))
+    day = db.Column(db.String(10)) 
+    time = db.Column(db.String(100))
+    location = db.Column(db.String(100)) 
+    instructor = db.Column(db.Integer, db.ForeignKey('Faculty.id'))
+
+class Taken(db.Model, UserMixin):
+    student = db.Column(db.Integer, db.ForeignKey('Student.id'), primary_key=True)
+    course = db.Column(db.Integer, db.ForeignKey('Courses.id'), primary_key=True)

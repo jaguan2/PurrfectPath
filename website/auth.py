4ff76053030db5fd955db343d3cf8c996f861659
graceynguyen <101678455+graceynguyen@users.noreply.github.com>
2024-04-21 14:52:34 -0400

    redirects

--- website/auth.py
+++ website/auth.py
@@ -69,6 +69,14 @@ def register():
 
   return render_template("register.html")
 
+@auth.route('/ret')
+def ret():
+  return render_template("index.html")
+
+@auth.route('/home')
+def home():
+  return redirect(url_for('views.myuser'))
+
 @auth.route('/search')
 def search():
   return render_template("schedule.html")
